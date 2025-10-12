0eca6e62117ccead218cd8125a5d914ab6397ca9
Daniel DDO <168279390+Daniel-DDO@users.noreply.github.com>
2025-10-12 11:07:58 -0300

    Update - Login e Token

--- src/main/java/com/ceara_sem_fome_back/security/JWTConfiguracao.java
+++ src/main/java/com/ceara_sem_fome_back/security/JWTConfiguracao.java
@@ -3,7 +3,6 @@ package com.ceara_sem_fome_back.security;
 import com.ceara_sem_fome_back.service.AdministradorService;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.http.HttpMethod;
 import org.springframework.security.authentication.AuthenticationManager;
 import org.springframework.security.authentication.dao.DaoAuthenticationProvider;
 import org.springframework.security.config.annotation.authentication.configuration.AuthenticationConfiguration;
@@ -12,6 +11,10 @@ import org.springframework.security.config.annotation.web.configuration.EnableWe
 import org.springframework.security.config.http.SessionCreationPolicy;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.security.web.util.matcher.AntPathRequestMatcher;
+import org.springframework.web.cors.CorsConfiguration;
+import org.springframework.web.cors.CorsConfigurationSource;
+import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
 
 @Configuration
 @EnableWebSecurity
@@ -44,11 +47,20 @@ public class JWTConfiguracao {
     }
 
     @Bean
-    public SecurityFilterChain securityFilterChain(HttpSecurity http, AuthenticationManager authManager) throws Exception {
+    public SecurityFilterChain securityFilterChain(HttpSecurity http,
+                                                   AuthenticationManager authManager) throws Exception {
+
+        AntPathRequestMatcher h2ConsoleMatcher = new AntPathRequestMatcher("/h2-console/**");
+        AntPathRequestMatcher admLoginMatcher = new AntPathRequestMatcher("/adm/login");
+        AntPathRequestMatcher loginPostMatcher = new AntPathRequestMatcher("/login", "POST");
+
         http
                 .csrf(csrf -> csrf.disable())
+                .headers(headers -> headers.frameOptions(frame -> frame.disable())) // necessário pro H2
                 .authorizeHttpRequests(auth -> auth
-                        .requestMatchers(HttpMethod.POST, "/login").permitAll()
+                        .requestMatchers(h2ConsoleMatcher).permitAll()
+                        .requestMatchers(admLoginMatcher).permitAll()
+                        .requestMatchers(loginPostMatcher).permitAll()
                         .anyRequest().authenticated()
                 )
                 .addFilter(new JWTAutenticarFilter(authManager))
@@ -57,5 +69,17 @@ public class JWTConfiguracao {
 
         return http.build();
     }
-}
 
+    @Bean
+    CorsConfigurationSource corsConfiguration() {
+        final UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
+
+        CorsConfiguration corsConfiguration = new CorsConfiguration().applyPermitDefaultValues();
+        corsConfiguration.addAllowedMethod("*");
+        corsConfiguration.addAllowedHeader("*");
+        corsConfiguration.addAllowedOriginPattern("*");
+
+        source.registerCorsConfiguration("/**", corsConfiguration);
+        return source;
+    }
+}
