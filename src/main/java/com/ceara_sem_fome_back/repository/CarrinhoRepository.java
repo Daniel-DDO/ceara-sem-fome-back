3d206c83f20e240a19ab951fdb461dac6c473a48
MiguelCR000 <miguel.cavalcanti@ufrpe.br>
2025-10-27 00:22:42 -0300

    Adicionando DTO e Repositórios para o Carrinho

--- src/main/java/com/ceara_sem_fome_back/repository/CarrinhoRepository.java
+++ src/main/java/com/ceara_sem_fome_back/repository/CarrinhoRepository.java
@@ -1,7 +1,10 @@
 package com.ceara_sem_fome_back.repository;
 
+import com.ceara_sem_fome_back.model.Carrinho;
+import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
 @Repository
-public interface CarrinhoRepository {
+public interface CarrinhoRepository extends JpaRepository<Carrinho, String> {
+    // Métodos de busca (ex: findByBeneficiarioId) podem ser adicionados aqui
 }
