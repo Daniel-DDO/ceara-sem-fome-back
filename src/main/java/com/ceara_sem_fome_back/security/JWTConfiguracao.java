e98ceed7c7e44ba32602cd60bc6c258244870ffa
Daniel DDO <168279390+Daniel-DDO@users.noreply.github.com>
2025-11-02 10:40:05 -0300

    Update CORS

--- src/main/java/com/ceara_sem_fome_back/security/JWTConfiguracao.java
+++ src/main/java/com/ceara_sem_fome_back/security/JWTConfiguracao.java
@@ -12,7 +12,6 @@ import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.http.SessionCreationPolicy;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.web.SecurityFilterChain;
-import org.springframework.security.web.util.matcher.AntPathRequestMatcher;
 import org.springframework.web.cors.CorsConfiguration;
 import org.springframework.web.cors.CorsConfigurationSource;
 import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
@@ -40,14 +39,12 @@ public class JWTConfiguracao {
     }
 
     @Bean
-    public SecurityFilterChain securityFilterChain(HttpSecurity http,
-                                                   AuthenticationManager authManager) throws Exception {
+    public SecurityFilterChain securityFilterChain(HttpSecurity http, AuthenticationManager authManager) throws Exception {
 
         http
-                .cors(cors -> cors.configurationSource(corsConfiguration()))
                 .csrf(csrf -> csrf.disable())
+                .cors(cors -> cors.configurationSource(corsConfigurationSource()))
                 .sessionManagement(sess -> sess.sessionCreationPolicy(SessionCreationPolicy.STATELESS))
-
                 .addFilter(new JWTAutenticarFilter(authManager, tokenSenha))
                 .addFilter(new JWTValidarFilter(authManager, tokenSenha, pessoaDetailsService))
 
@@ -59,42 +56,156 @@ public class JWTConfiguracao {
                 )
 
                 .authorizeHttpRequests(auth -> auth
-                                .requestMatchers(
-                                        new AntPathRequestMatcher("/*/iniciar-cadastro"),
-                                        new AntPathRequestMatcher("/*/login"),
-                                        new AntPathRequestMatcher("/token/confirmar-cadastro"),
-                                        new AntPathRequestMatcher("/**/all"),
-                                        new AntPathRequestMatcher("/version"),
-                                        new AntPathRequestMatcher("/health"),
-                                        new AntPathRequestMatcher("/**/meu-perfil"),
-                                        new AntPathRequestMatcher("/**/estabelecimento/"),
-                                        new AntPathRequestMatcher("estabelecimento/**"),
-                                        new AntPathRequestMatcher("/**/"),
-                                        new AntPathRequestMatcher("/auth/**"),
-                                        new AntPathRequestMatcher("/**/bairro/**"),
-                                        new AntPathRequestMatcher("/**/municipio/**"),
-                                        new AntPathRequestMatcher("/beneficiario/cadastrar-endereco"),
-                                        new AntPathRequestMatcher("/estabelecimento/cadastrar-endereco")
-                                ).permitAll()
-                                .anyRequest().authenticated()
-                        //não remover esse comentário!
-                        //.anyRequest().permitAll() //para testes, permitir tudo
+                        .requestMatchers("/**").permitAll()
+                        .anyRequest().permitAll()
                 );
 
         return http.build();
     }
 
     @Bean
-    public CorsConfigurationSource corsConfiguration() {
+    public CorsConfigurationSource corsConfigurationSource() {
         CorsConfiguration cors = new CorsConfiguration();
-        cors.setAllowedOrigins(List.of("http://localhost:5173"));
-        cors.setAllowedMethods(List.of("GET", "POST", "PUT", "DELETE", "OPTIONS"));
+
+        cors.setAllowedOriginPatterns(List.of("*"));
+        cors.setAllowedMethods(List.of("GET", "POST", "PUT", "DELETE", "OPTIONS", "PATCH"));
         cors.setAllowedHeaders(List.of("*"));
+        cors.setExposedHeaders(List.of("Authorization", "Content-Type"));
         cors.setAllowCredentials(true);
 
         UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
         source.registerCorsConfiguration("/**", cors);
+
         return source;
     }
-
 }
+
+
+//
+//package com.ceara_sem_fome_back.security;
+//
+//import com.ceara_sem_fome_back.security.Handler.LoggingLogoutSuccessHandler;
+//import com.ceara_sem_fome_back.service.PessoaDetailsService;
+//import org.springframework.beans.factory.annotation.Autowired;
+//import org.springframework.beans.factory.annotation.Value;
+//import org.springframework.context.annotation.Bean;
+//import org.springframework.context.annotation.Configuration;
+//import org.springframework.security.authentication.AuthenticationManager;
+//import org.springframework.security.config.annotation.authentication.configuration.AuthenticationConfiguration;
+//import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+//import org.springframework.security.config.http.SessionCreationPolicy;
+//import org.springframework.security.crypto.password.PasswordEncoder;
+//import org.springframework.security.web.SecurityFilterChain;
+//import org.springframework.security.web.util.matcher.AntPathRequestMatcher;
+//import org.springframework.web.cors.CorsConfiguration;
+//import org.springframework.web.cors.CorsConfigurationSource;
+//import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
+//
+//import java.util.List;
+//
+//@Configuration
+//public class JWTConfiguracao {
+//
+//    @Value("${api.guid.token.senha}")
+//    private String tokenSenha;
+//
+//    @Autowired
+//    private LoggingLogoutSuccessHandler loggingLogoutSuccessHandler;
+//
+//    @Autowired
+//    private PessoaDetailsService pessoaDetailsService;
+//
+//    @Autowired
+//    private PasswordEncoder passwordEncoder;
+//
+//    @Bean
+//    public AuthenticationManager authenticationManager(AuthenticationConfiguration authConfig) throws Exception {
+//        return authConfig.getAuthenticationManager();
+//    }
+
+
+//    @Bean
+//    public SecurityFilterChain securityFilterChain(HttpSecurity http,
+//                                                   AuthenticationManager authManager) throws Exception {
+//
+//        http
+//                .cors(cors -> cors.configurationSource(corsConfiguration()))
+//                .csrf(csrf -> csrf.disable())
+//                .sessionManagement(sess -> sess.sessionCreationPolicy(SessionCreationPolicy.STATELESS))
+//
+//                .addFilter(new JWTAutenticarFilter(authManager, tokenSenha))
+//                .addFilter(new JWTValidarFilter(authManager, tokenSenha, pessoaDetailsService))
+//
+//                .logout(logout -> logout
+//                        .logoutUrl("/auth/logout")
+//                        .logoutSuccessHandler(loggingLogoutSuccessHandler)
+//                        .invalidateHttpSession(true)
+//                        .deleteCookies("JSESSIONID")
+//                )
+//
+//                .authorizeHttpRequests(auth -> auth
+//                                .requestMatchers(
+//                                        new AntPathRequestMatcher("/*/iniciar-cadastro"),
+//                                        new AntPathRequestMatcher("/*/login"),
+//                                        new AntPathRequestMatcher("/token/confirmar-cadastro"),
+//                                        new AntPathRequestMatcher("/**/all"),
+//                                        new AntPathRequestMatcher("/version"),
+//                                        new AntPathRequestMatcher("/health"),
+//                                        new AntPathRequestMatcher("/**/meu-perfil"),
+//                                        new AntPathRequestMatcher("/**/estabelecimento/"),
+//                                        new AntPathRequestMatcher("estabelecimento/**"),
+//                                        new AntPathRequestMatcher("/**/"),
+//                                        new AntPathRequestMatcher("/auth/**"),
+//                                        new AntPathRequestMatcher("/**/bairro/**"),
+//                                        new AntPathRequestMatcher("/**/municipio/**"),
+//                                        new AntPathRequestMatcher("/beneficiario/cadastrar-endereco"),
+//                                        new AntPathRequestMatcher("/estabelecimento/cadastrar-endereco")
+//                                ).permitAll()
+//                                .anyRequest().authenticated()
+//                        //não remover esse comentário!
+//                        //.anyRequest().permitAll() //para testes, permitir tudo
+//                );
+//
+//        return http.build();
+//    }
+//
+//    @Bean
+//    public SecurityFilterChain securityFilterChain(HttpSecurity http, AuthenticationManager authManager) throws Exception {
+//
+//        http.cors().and()
+//                .csrf(csrf -> csrf.disable())
+//                .sessionManagement(sess -> sess.sessionCreationPolicy(SessionCreationPolicy.STATELESS))
+//
+//                .addFilter(new JWTAutenticarFilter(authManager, tokenSenha))
+//                .addFilter(new JWTValidarFilter(authManager, tokenSenha, pessoaDetailsService))
+//
+//                .logout(logout -> logout
+//                        .logoutUrl("/auth/logout")
+//                        .logoutSuccessHandler(loggingLogoutSuccessHandler)
+//                        .invalidateHttpSession(true)
+//                        .deleteCookies("JSESSIONID")
+//                )
+//
+//                .authorizeHttpRequests(auth -> auth
+//                                .requestMatchers("/**").permitAll()
+//                );
+//
+//        return http.build();
+//    }
+//
+//
+//    @Bean
+//    public CorsConfigurationSource corsConfiguration() {
+//        CorsConfiguration cors = new CorsConfiguration();
+//
+//        cors.setAllowedOriginPatterns(List.of("*"));
+//        cors.setAllowedMethods(List.of("GET", "POST", "PUT", "DELETE", "OPTIONS", "PATCH"));
+//        cors.setAllowedHeaders(List.of("*"));
+//        cors.setAllowCredentials(true);
+//        cors.setExposedHeaders(List.of("Authorization", "Content-Type"));
+//
+//        UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
+//        source.registerCorsConfiguration("/**", cors);
+//        return source;
+//    }
+//}
