004f9558edbdd86d424799d91f0fa2027cf39532
Daniel DDO <168279390+Daniel-DDO@users.noreply.github.com>
2025-11-02 12:14:37 -0300

    Update CORS

--- src/main/java/com/ceara_sem_fome_back/security/JWTConfiguracao.java
+++ src/main/java/com/ceara_sem_fome_back/security/JWTConfiguracao.java
@@ -14,6 +14,8 @@ import org.springframework.security.web.SecurityFilterChain;
 import org.springframework.security.web.authentication.logout.LogoutSuccessHandler;
 import org.springframework.security.web.util.matcher.AntPathRequestMatcher;
 
+import java.util.List;
+
 @Configuration
 public class JWTConfiguracao {
 
@@ -42,7 +44,13 @@ public class JWTConfiguracao {
 
         http
                 .csrf(csrf -> csrf.disable())
-                .cors(cors -> {})
+                .cors(cors -> cors.configurationSource(request -> {
+                    var corsConfig = new org.springframework.web.cors.CorsConfiguration();
+                    corsConfig.setAllowedOrigins(List.of("http://localhost:5173"));
+                    corsConfig.setAllowedMethods(List.of("GET","POST","PUT","PATCH","DELETE"));
+                    corsConfig.setAllowedHeaders(List.of("*"));
+                    return corsConfig;
+                }))
                 .sessionManagement(sess -> sess.sessionCreationPolicy(SessionCreationPolicy.STATELESS))
 
                 .addFilter(new JWTAutenticarFilter(authManager, tokenSenha))
