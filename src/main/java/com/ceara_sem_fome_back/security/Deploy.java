2f3e42837691286c648eb71fc8cae4ec69e2a475
Daniel DDO <168279390+Daniel-DDO@users.noreply.github.com>
2025-11-07 20:44:18 -0300

    Update server link to new DigitalOcean URL

--- src/main/java/com/ceara_sem_fome_back/security/Deploy.java
+++ src/main/java/com/ceara_sem_fome_back/security/Deploy.java
@@ -4,5 +4,5 @@ import lombok.Getter;
 
 public class Deploy {
     @Getter
-    public static String linkServidor = "http://ceara-sem-fome-back-env.eba-gde6qmc4.us-east-2.elasticbeanstalk.com";
+    public static String linkServidor = "https://ceara-raiz-srb9k.ondigitalocean.app";
 }
