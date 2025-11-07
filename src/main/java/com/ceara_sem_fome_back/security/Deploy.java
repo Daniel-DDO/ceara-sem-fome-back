047030600fea233b55a548866fc4cd01c3ac3059
Daniel DDO <168279390+Daniel-DDO@users.noreply.github.com>
2025-11-07 11:09:28 -0300

    Update Correção

---
+++ src/main/java/com/ceara_sem_fome_back/security/Deploy.java
@@ -0,0 +1,8 @@
+package com.ceara_sem_fome_back.security;
+
+import lombok.Getter;
+
+public class Deploy {
+    @Getter
+    public static String linkServidor = "http://ceara-sem-fome-back-env.eba-gde6qmc4.us-east-2.elasticbeanstalk.com";
+}
