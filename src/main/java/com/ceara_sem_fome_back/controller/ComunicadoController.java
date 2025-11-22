4d550acee0e11fef87d837ee4f2c37ae4fec1837
Daniel DDO <168279390+Daniel-DDO@users.noreply.github.com>
2025-11-22 17:46:12 -0300

    Atualizado - Comunicados

--- src/main/java/com/ceara_sem_fome_back/controller/ComunicadoController.java
+++ src/main/java/com/ceara_sem_fome_back/controller/ComunicadoController.java
@@ -8,7 +8,8 @@ import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 
-@RestController("/comunicado")
+@RestController
+@RequestMapping("/comunicado")
 public class ComunicadoController {
 
     @Autowired
